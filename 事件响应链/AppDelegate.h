d198a19a48a34deb63c1f123c209ffe6f86664f4
zh dk <zhdk@zhdemacbook-pro.local>
2017-08-24 16:14:20 +0800

    Initial Commit

---
+++ 事件响应链/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  事件响应链
+//
+//  Created by zh dk on 2017/8/24.
+//  Copyright © 2017年 zh dk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
